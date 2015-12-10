eeb6abc2ee9c883459b7280db391565c11325fdd
AronVanAmmers <aron@blockstars.io>
2015-12-10 17:28:59 +0100

    Making offers + unit tests

--- contracts/ProposalRegistry.sol
+++ contracts/ProposalRegistry.sol
@@ -2,7 +2,40 @@
  * Offer to respond to a proposal.
  */
 contract Offer {
+    /**
+     * The seller who made this offer.
+     */
+    address public sellerAddress;
 
+    /**
+     * Price offered at the minimum amount.
+     */
+    uint public price;
+
+    /**
+     * Minimum amount of products to be bought.
+     */
+    uint public minimumAmount;
+
+    function setPrice(uint p){
+        if(tx.origin != sellerAddress)
+            return;
+
+        price = p;
+    }
+
+    function setMinimumAmount(uint a){
+        if(tx.origin != sellerAddress)
+            return;
+
+        minimumAmount = a;
+    }
+
+    function Offer(address sa, uint p, uint ma){
+        sellerAddress = sa;
+        price = p;
+        minimumAmount = ma;
+    }
 }
 
 
@@ -52,6 +85,18 @@ contract Proposal {
      */
     mapping(address => uint) public backerIndexByAddress;
 
+    /**
+     * Prospective buyers backing this proposal.
+     */
+    mapping(uint => Offer) public offers;
+    uint public offerIndex;
+
+    /**
+     * Index to help access the backer mapping.
+     */
+    mapping(address => uint) public offerIndexByAddress;
+
+
     function Proposal(string pn, string pd, uint mp, string ed, string udd) {
         productName = pn;
         productDescription = pd;
@@ -85,20 +130,44 @@ contract Proposal {
     /**
      * Make an offer
      */
-    function offer(uint price) {
+    function offer(uint price, uint minimumAmount) returns (Offer o){
         // No free offers allowed. Also for safety purposes (empty might end up as 0).
-        if(price = 0) return;
+        if(price == 0) return;
         if(price > maxPrice) return;
+        if(minimumAmount == 0) return;
 
-        // TODO: implement
+        // Check for an existing offer of this seller
+        if(offerIndexByAddress[tx.origin] > 0) {
+            // Because this is a related contract, we can't set public properties.
+            // TODO: check if there are default setters, maybe set_price() or similar?
+            offers[offerIndexByAddress[tx.origin]].setPrice(price);
+            offers[offerIndexByAddress[tx.origin]].setMinimumAmount(minimumAmount);
+            return;
+        }
 
+        offerIndex++;
 
+        offerIndexByAddress[tx.origin] = offerIndex;
+
+        o = new Offer(tx.origin, price, minimumAmount);
+        offers[offerIndex] = o;
+
+        return o;
     }
 
     /**
      * Cancel the offer of a seller if the proposal is still open.
      */
     function cancelOffer(){
+        // TODO
+    }
+
+    /**
+     * Attempt to close the proposal if the closing conditions are met:
+     * - the end date has been reached
+     * - a valid offer has been made
+     */
+    function close(){
 
     }
 }
@@ -123,7 +192,8 @@ contract ProposalRegistry {
     mapping (uint=>Proposal) public proposals;
     uint proposalIndex;
 
-    function addProposal(string productName, string description, uint maxPrice, string endDate, string ultimateDeliveryDate) returns (Proposal p) {               proposalIndex++;
+    function addProposal(string productName, string description, uint maxPrice, string endDate, string ultimateDeliveryDate) returns (Proposal p) {
+        proposalIndex++;
 
         p = new Proposal(productName, description, maxPrice, endDate, ultimateDeliveryDate);
         proposals[proposalIndex] = p;
---
+++ test/contracts/testProposalOffers.ts
@@ -0,0 +1,121 @@
+﻿import assert = require('assert');
+import web3config = require('./web3config');
+import fs = require('fs');
+
+var web3plus = web3config.web3plus;
+var web3 = web3plus.web3;
+
+describe("ProposalRegistry", () => {
+    /**
+     * The Solidity web3 contract.
+     */
+    var registryContract;
+
+    var proposalContractDefinition;
+    var offerContractDefinition;
+
+    var timeBeforeDeployment: number;
+    var timeAfterDeployment: number;
+
+    var testRegistryName = "BuyCo proposals registry";
+
+    before(function (done) {
+        // It can take quite a while til transactions are processed.
+        this.timeout(180000);
+
+        timeBeforeDeployment = Date.now();
+
+        web3config.createWeb3();
+
+        web3plus.deployContractFromFile("ProposalRegistry.sol",
+            "ProposalRegistry",
+            true,
+            function (err, res) {
+                timeAfterDeployment = Date.now();
+                registryContract = res;
+
+                // Save the sub contract definitions to variables for easy access.
+                proposalContractDefinition = registryContract.allContractTypes.Proposal.contractDefinition;
+                offerContractDefinition = registryContract.allContractTypes.Offer.contractDefinition;
+
+                done(err);
+            },
+            testRegistryName);
+    });
+
+    it("should add offers for a proposal", function (done) {
+        // It can take quite a while til transactions are processed.
+        this.timeout(180000);
+
+        var name1 = "iPhone 6S";
+        var price1 = 10100;
+        var amount1 = 550;
+
+        // Currently all transactions are sent from a single address. Hence the "backer" is
+        // also that address.
+        var sellerAddress1 = web3.eth.coinbase;
+
+        var proposalContract;
+
+        registryContract.addProposal(name1, "A very special product", price1, "2016-03-01", "2016-05-01", { gas: 2500000 })
+            .then(web3plus.promiseCommital)
+            .then(function testGetProposal(tx) {
+                var newProposalAddress = registryContract.proposals(1);
+
+                proposalContract = proposalContractDefinition.at(newProposalAddress);
+
+                return proposalContract.offer(price1, amount1, { gas: 2500000 });
+            })
+            .then(web3plus.promiseCommital)
+            .then(function testGetOffer(tx) {
+                var newOfferAddress = proposalContract.offers(1);
+                var newOffer = offerContractDefinition.at(newOfferAddress);
+
+                assert.equal(newOffer.sellerAddress(), sellerAddress1);
+                assert.equal(newOffer.price().toNumber(), price1);
+                assert.equal(newOffer.minimumAmount().toNumber(), amount1);
+
+                // Add more "offers". This should update the amount, not add more backers, as we're backing
+                // from the same address.
+                proposalContract.offer(1, 1, { gas: 2500000 });
+                proposalContract.offer(2, 2, { gas: 2500000 });
+                proposalContract.offer(3, 3, { gas: 2500000 });
+                proposalContract.offer(4, 4, { gas: 2500000 });
+                proposalContract.offer(5, 5, { gas: 2500000 });
+                proposalContract.offer(6, 6, { gas: 2500000 });
+                proposalContract.offer(7, 7, { gas: 2500000 });
+                return proposalContract.offer(8, 8, { gas: 2500000 });
+            })
+            .then(web3plus.promiseCommital)
+            .then(function testGetLatestOffer(tx) {
+                var newOfferAddress = proposalContract.offers(1);
+                var newOffer = offerContractDefinition.at(newOfferAddress);
+
+                var offerIndex = proposalContract.offerIndex().toNumber();
+
+                assert.equal(offerIndex, 1);
+
+                // Backer address should be unchanged.
+                assert.equal(newOffer.sellerAddress(), sellerAddress1);
+
+                var newPrice = newOffer.price().toNumber();
+                var newAmount = newOffer.minimumAmount().toNumber();
+
+                // Because there is no guarantee for the sequence in which transaction are processed,
+                // we don't know for sure what the current amount should be. We do however now that it 
+                // should be between 1 and 8. Usually it will be 8.
+                assert.ok(newPrice <= 8);
+                assert.ok(newPrice >= 1);
+
+                // Whichever of the transactions was the final one, the price should be equal
+                // to the amount.
+                assert.equal(newAmount, newPrice);
+
+                done();
+            })
+            .catch((reason) => {
+                done(reason);
+            });
+    });
+
+});
