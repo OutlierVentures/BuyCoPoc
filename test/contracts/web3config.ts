b84439dd4062252a43c343819160377a436aa4e6
AronVanAmmers <aron@blockstars.io>
2015-12-28 16:19:38 +0100

    Doc

--- test/contracts/web3config.ts
+++ test/contracts/web3config.ts
@@ -4,6 +4,13 @@ import path = require('path');
 
 export function createWeb3() {
     var cs = new configurationService.ConfigurationService();
+
+    // This file gets called from various locations. When running tests from
+    // command line using `npm test`, the path is the project root. When
+    // running from the Visual Studio test runner, the path is the file
+    // path of the test itself. Hence we can't use __dirname to find the config
+    // file, and use __filename (the path of this file) to get the the location 
+    // of the config file relative to this file.
     cs.basePath = path.resolve(path.dirname(__filename), "../../") + "/";
     var config = cs.getConfiguration();
 
