6816e387f5bcffda6f39ca0e0e4b9f9ec9852ae2
AronVanAmmers <aron@blockstars.io>
2016-02-10 17:05:26 +0100

    Add test for simple contract deployment

---
+++ test/contracts/testContractDeployment.ts
@@ -0,0 +1,57 @@
+﻿import assert = require('assert');
+import web3config = require('./web3config');
+import fs = require('fs');
+
+import contractInterfaces = require('../../contracts/contractInterfaces');
+import contractService = require('../../services/contractService');
+import serviceFactory = require('../../services/serviceFactory');
+
+var web3plus = web3config.web3plus;
+var web3 = web3plus.web3;
+
+describe("ContractDeployment", () => {    
+    var timeBeforeDeployment: number;
+    var timeAfterDeployment: number;
+
+    before(function (done) {
+        //this.timeout(10000);
+        web3config.createWeb3();
+
+        done();
+    });
+
+    it("should deploy an Offer contract", function (done) {
+        this.timeout(60000);
+
+
+        timeBeforeDeployment = Date.now();
+
+        web3plus.deployContractFromFile("ProposalRegistry.sol",
+            "Offer",
+            true,
+            function (err, res) {
+                timeAfterDeployment = Date.now();
+
+                done(err);
+            },
+            web3plus.web3.eth.coinbase, 1, 1);
+    });
+
+    it("should deploy a ProposalRegistry contract", function (done) {
+        this.timeout(60000);
+
+        timeBeforeDeployment = Date.now();
+
+        web3plus.deployContractFromFile("ProposalRegistry.sol",
+            "ProposalRegistry",
+            true,
+            function (err, res) {
+                timeAfterDeployment = Date.now();
+
+                done(err);
+            },
+            "Testing proposal registry");
+    });
+
+
+});
